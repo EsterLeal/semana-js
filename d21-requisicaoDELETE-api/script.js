5843f1cf4623849140f911fc29eb13bf2122d82a
EsterLeal <e_esterribeiro@hotmail.com>
2019-05-09 01:13:44 -0300

    atualicação

--- d21-requisicaoDELETE-api/script.js
+++ d21-requisicaoDELETE-api/script.js
@@ -4,45 +4,53 @@ authors.setAttribute('class', 'authors');
 caixa.appendChild(authors);
 
 fetch('https://reqres.in/api/users')
-.then((response) => {
-    return response.json();
-})
-.then((info) => {
-    console.log(info);
-
-    info.data.forEach(pessoas=> {
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        card.setAttribute('id', pessoas.id);
-        authors.appendChild(card);
-        
-        const nome = document.createElement('h2');
-        nome.textContent = pessoas.first_name + " " + pessoas.last_name;
-        card.appendChild(nome);
-        
-        const email = document.createElement('p');
-        email.textContent = pessoas.email;
-        card.appendChild(email);
-
-        const botao = document.createElement("button");
-        botao.innerHTML = "✖";
-        botao.setAttribute('data-id', pessoas.id);
-        card. appendChild(botao);
-
-        botao.addEventListener("click", () => {
-            console.log("EAE");
-        })
-
-        // const avatar = document.createElement('img');
-        // avatar.setAttribute ("src", pessoas.avatar);
-        // authors.appendChild(avatar);
-                
+    .then((response) => {
+        return response.json();
     })
-})
-.catch((erro) => {
-    console.log(erro);
-})
+    .then((info) => {
+        console.log(info);
+
+        info.data.forEach(pessoas => {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+            card.setAttribute('id', 'pessoas.id');
+            authors.appendChild(card);
+
+            const nome = document.createElement('h2');
+            nome.textContent = pessoas.first_name + " " + pessoas.last_name;
+            card.appendChild(nome);
+
+            const email = document.createElement('p');
+            email.textContent = pessoas.email;
+            card.appendChild(email);
+
+            const botao = document.createElement('button');
+            botao.innerHTML = "✖";
+            botao.setAttribute('data-id', pessoas.id);
+            card.appendChild(botao);
+
+            botao.addEventListener('click', () => {
+                const thisCard = botao.parentElement;
+                const cardPai = thisCard.parentElement;
+
+                fetch('https://reqres.in/api/users', {
+                    method: 'DELETE',
+                    headers: {
+                        'Accepts': 'Applications/json',
+                        'Content-Type': 'Applications/json'
+                    },
+                    body: JSON.stringify({
+                        'id': botao.getAttribute('data-id')
+                    })
+                })
+                    .then(() => {
+                        cardPai.removeChild(thisCard);
+                    })
+                    .catch((erro => {
+                        console.log(erro)
+                    }))
 
 
-
-
+            })
+        })
+    })
